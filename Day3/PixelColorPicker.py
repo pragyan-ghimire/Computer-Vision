bcd472455cf481afe358ea6ca1777215780d1671
pragyan <ghimirepragyan100@gmail.com>
2025-06-18 21:40:02 +0545

    optimized pixel color picker

--- Day3/PixelColorPicker.py
+++ Day3/PixelColorPicker.py
@@ -2,28 +2,23 @@ import cv2 as cv
 
 img = cv.imread('../Resources/Photos/lady.jpg')
 
-# 
 def mouse_click(event, x, y, flags, param):
-        
+    img_copy = param.copy()
     # to check if right mouse
     if event == cv.EVENT_RBUTTONDOWN:       
         # In OpenCV, the y-coordinate (row) comes first, then x-coordinate (column).
-        color = img[y, x] # y = row, x= column
+        color = img_copy[y, x] # y = row, x= column
         font = cv.FONT_HERSHEY_COMPLEX
-        
-        cv.putText(img, f"BGR: {color[0]}, {color[1]}, {color[2]}", (x, y),
-                    font, 1, 
-                    (0, 0, 0),
-                    2)
-        cv.imshow('Image', img)
+        text = f"BGR: {color[0]}, {color[1]}, {color[2]}"
+        text_pos = (min(x, img.shape[1] - 300), max(y, 20))
+        cv.putText(img_copy, text, text_pos, font, 1, (255, 255, 255), 1)
+        cv.imshow('Image', img_copy)
         # print(f"Coordinate pressed: {x, y}")
         
 if img is None:
     print("Image was not loaded succesfully.")
 else:
     cv.imshow('Image', img)
-
-cv.setMouseCallback('Image', mouse_click)
-
-cv.waitKey(0)
-cv.destroyAllWindows() # close all the opened windows
+    cv.setMouseCallback('Image', mouse_click, img)
+    cv.waitKey(0)
+    cv.destroyAllWindows() # close all the opened windows
